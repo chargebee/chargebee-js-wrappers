f0414c298145cdb7db039a35b0e37667fa238906
Abhishek Rai <>
2021-03-19 17:55:56 +0530

    Provider component added to support SSR | cbInstance validation added

--- chargebee-js-angular/projects/chargebee-js-angular-wrapper/src/lib/chargebee-js-angular-wrapper.module.ts
+++ chargebee-js-angular/projects/chargebee-js-angular-wrapper/src/lib/chargebee-js-angular-wrapper.module.ts
@@ -3,11 +3,14 @@ import { CardFieldDirective } from './directives/card-field.directive';
 import { CvvFieldDirective } from './directives/cvv-field.directive';
 import { NumberFieldDirective } from './directives/number-field.directive';
 import { ExpiryFieldDirective } from './directives/expiry-field.directive';
+import { Provider } from './directives/provider.directive';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [CardFieldDirective, CvvFieldDirective, NumberFieldDirective, ExpiryFieldDirective],
+  declarations: [CardFieldDirective, CvvFieldDirective, NumberFieldDirective, ExpiryFieldDirective, Provider],
   imports: [
+    CommonModule
   ],
-  exports: [CardFieldDirective, CvvFieldDirective, NumberFieldDirective, ExpiryFieldDirective]
+  exports: [CardFieldDirective, CvvFieldDirective, NumberFieldDirective, ExpiryFieldDirective, Provider]
 })
 export class ChargebeeJsAngularWrapperModule { }
