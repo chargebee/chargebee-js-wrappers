ae424d0443e437e5daa912126f78aede363a2aa8
cb-farhanh <farhan.h@chargebee.com>
2022-11-18 11:00:12 +0530

    CHKOUTENGG-35404 build: add typescript compilation in viteconfig

--- chargebee-js-vue/vite.config.js
+++ chargebee-js-vue/vite.config.js
@@ -2,11 +2,12 @@
 const path = require('path')
 const { defineConfig } = require('vite')
 const vue = require('@vitejs/plugin-vue');
+const typescript2 = require('rollup-plugin-typescript2');
 
 module.exports = defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
+      entry: path.resolve(__dirname, 'src/index.jts'),
       name: 'chargebee-js-vue-wrapper',
       fileName: (format) => `chargebee-js-vue-wrapper.${format}.js`,
       formats: ['cjs', 'es' ]
@@ -14,6 +15,10 @@ module.exports = defineConfig({
     rollupOptions: {
       external: ['vue'],
       plugins: [
+        {
+          ...typescript2(),
+          apply: 'build',
+        }
       ],
       output: {
         exports: "named",
