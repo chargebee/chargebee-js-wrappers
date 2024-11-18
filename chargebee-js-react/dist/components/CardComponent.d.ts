430b5d86d4f65e4baf4d50df61967eef3bfca703
Dinesh <dinesh@chargebee.com>
2024-11-18 12:26:13 +0530

    chore: removes dist

--- chargebee-js-react/dist/components/CardComponent.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import ComponentGroup, { ChargebeeComponentProps } from './ComponentGroup';
-declare const _default: React.ForwardRefExoticComponent<ChargebeeComponentProps & React.RefAttributes<ComponentGroup>>;
-export default _default;
--- chargebee-js-react/dist/utils/index.d.ts
+++
@@ -1,7 +0,0 @@
-interface KeyObject extends Object {
-    [key: number]: any;
-}
-export declare function isEqual(left: KeyObject, right: KeyObject): Array<any> | boolean;
-export declare function genUUID(): string;
-export declare function validateCbInstance(cbInstance: any): boolean;
-export {};
