e9eaa48fcb8ff6203369c9fee3117e1909517a3e
cb-sourabh <sourabh.yadav@chargebee.com>
2025-01-03 17:29:31 +0530

    code cleanup and updating changelog

--- chargebee-js-angular/projects/chargebee-js-angular-wrapper/src/public-api.ts
+++ chargebee-js-angular/projects/chargebee-js-angular-wrapper/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of chargebee-js-angular-wrapper
  */
 
-//export * from './lib/chargebee-js-angular-wrapper.module';
 export * from './lib/directives/card-field.directive';
 export * from './lib/directives/number-field.directive';
 export * from './lib/directives/expiry-field.directive';
