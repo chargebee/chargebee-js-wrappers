ee88b70889068c2c97e6327ccaab049d58cb10b1
Dinesh <dinesh@chargebee.com>
2025-01-06 10:30:25 +0530

    chore: cleanup

--- chargebee-js-angular/dist/chargebee-js-angular-wrapper/utils/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { SimpleChanges } from '@angular/core';
-export declare function isEqual(left: any, right: any): boolean;
-export declare function getPropChanges(changes: SimpleChanges, props: string[]): {
-    hasChanged: boolean;
-    currentOptions: object;
-    prevOptions: object;
-};
-export declare function validateCbInstance(cbInstance: any): boolean;
-//# sourceMappingURL=index.d.ts.map
